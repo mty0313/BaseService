a8a61ce1142c54ec3891760b39edff763206d37a
mty <mty0313@126.com>
2021-04-16 16:09:29 +0800

    解决基本依赖和配置

---
+++ src/main/java/top/mty/BaseServiceApplication.java
@@ -0,0 +1,16 @@
+package top.mty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author mty
+ * @Date 2021/4/16
+ * @Description application
+ */
+@SpringBootApplication
+public class BaseServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseServiceApplication.class, args);
+    }
+}
---
+++ src/main/java/top/mty/Controller/TestController.java
@@ -0,0 +1,25 @@
+package top.mty.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author mty
+ * @Date 2021/4/16
+ * @Description test controller
+ */
+@RestController
+public class TestController {
+
+    Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @PostMapping("/testController/{val}")
+    public String testController(@PathVariable String val) {
+        logger.info("get val: {}", val);
+        return val;
+    }
+
+}
